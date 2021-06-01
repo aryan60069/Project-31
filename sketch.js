4b883cd457307f2f47d0a33d26a3078e6b56c83c
aryan60069 <78200051+aryan60069@users.noreply.github.com>
2021-06-01 20:57:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,74 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Events = Matter.Events;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  //create division objects
+  for (var k = 0; k <=width; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  //create 1st row of plinko objects
+  for (var j = 75; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,75));
+  }
+
+  //create 2nd row of plinko objects
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,175));
+  }
+
+  //create 3rd row of plinko objects
+  for (var j = 75; j <=width; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,275));
+  }
+  
+  //create 4th row of plinko objects
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,375));
+  }
+
+  particles.push(new Particle(random(width/2-100,width/2 + 100),10,10));    
+}
+ 
+function draw() {
+  background("black");
+ 
+  Engine.update(engine);
+  ground.display();
+  
+  //display the plinkos 
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();   
+  }
+   
+  //display the divisions
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+
+  for (var p = 0; p < particles.length; p++){
+    particles[p].display();
+  }
+
+//display particle objects
+if(frameCount%60===0){
+  particles.push(new Particle(random(width/2-200, width/2+200), 10, 10));
+}
+}
